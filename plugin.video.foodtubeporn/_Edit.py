440d3804a7c855fd560b135dc181dd8a40d1a1d6
JustMev <support@justmeroms.com>
2016-11-12 17:47:18 +1100

    minor change

---
+++ plugin.video.foodtubeporn/_Edit.py
@@ -0,0 +1,4 @@
+import xbmcaddon
+
+MainBase = 'https://raw.githubusercontent.com/JustMev/foodtubeporn/master/home2.txt?token=AOJJ0pUjfF44Ft-wxTVKiov0LS9QLebqks5YLqZ1wA%3D%3D'
+addon = xbmcaddon.Addon('plugin.video.foodtubeporn')
